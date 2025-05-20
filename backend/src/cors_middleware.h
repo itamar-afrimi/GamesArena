94a6628becbb71d051ea479593b36dffce54e478
itamar-afrimi <afrimi2000@gmail.com>
2025-05-20 15:36:50 +0300

    change global for web test

--- backend/src/cors_middleware.h
+++ backend/src/cors_middleware.h
@@ -3,7 +3,7 @@ struct CORS {
     // need to change "FRONTEND_URL" to local URL when testing locally
     void before_handle(crow::request& req, crow::response& res, context&) {
         //res.set_header("Access-Control-Allow-Origin", std::getenv("FRONTEND_URL"));
-        res.set_header("Access-Control-Allow-Origin", "http://localhost:5173");
+        res.set_header("Access-Control-Allow-Origin", std::getenv("FRONTEND_URL"));
         res.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.set_header("Access-Control-Allow-Headers", "Content-Type");
 
@@ -17,7 +17,7 @@ struct CORS {
     void after_handle(crow::request&, crow::response& res, context&) {
         // Do NOT add headers again here
         //res.set_header("Access-Control-Allow-Origin", std::getenv("FRONTEND_URL"));
-        res.set_header("Access-Control-Allow-Origin", "http://localhost:5173");
+        res.set_header("Access-Control-Allow-Origin", std::getenv("FRONTEND_URL"));
         res.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.set_header("Access-Control-Allow-Headers", "Content-Type");
     }
