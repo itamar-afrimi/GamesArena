35a7b6bb0486db67cc4e8f49b8978a293022c383
itamar-afrimi <afrimi2000@gmail.com>
2025-05-22 13:46:58 +0300

    change middleware for automatic find origin for cor

--- backend/src/cors_middleware.h
+++ backend/src/cors_middleware.h
@@ -5,13 +5,17 @@ struct CORS {
     // need to change "FRONTEND_URL" to local URL when testing locally
     void before_handle(crow::request& req, crow::response& res, context&) {
         //res.set_header("Access-Control-Allow-Origin", std::getenv("FRONTEND_URL"));
+
+        const char* allowed_origins_env = std::getenv("CORS_ALLOWED_ORIGINS");
+        std::string allowed_origins = allowed_origins_env ? allowed_origins_env : "*";
+
         
-        std::string origin = req.get_header_value("Origin");
+        // std::string origin = req.get_header_value("Origin");
         // ctx.origin = origin;
         // if (origin == "http://localhost:5173/" || origin == "https://games-arena-4bfe.vercel.app") {
         //     res.set_header("Access-Control-Allow-Origin", origin);
         // }
-        res.set_header("Access-Control-Allow-Origin", origin);
+        res.set_header("Access-Control-Allow-Origin", allowed_origins);
         res.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.set_header("Access-Control-Allow-Headers", "Content-Type");
 
@@ -31,15 +35,11 @@ struct CORS {
         // // }
         // std::cout << "----------------------------------" << std::endl;
         // std::string origin = req.get_header_value("Origin");
-        auto it = res.headers.find("X-Set-Origin");
-        if (it != res.headers.end()) {
-            std::cout << it->second << std::endl;
-            std::string origin = it->second;
-            res.set_header("Access-Control-Allow-Origin", origin);
-            res.headers.erase(it); // Remove custom header
-        }
+        const char* allowed_origins_env = std::getenv("CORS_ALLOWED_ORIGINS");
+        std::string allowed_origins = allowed_origins_env ? allowed_origins_env : "*";
+
         
-        res.set_header("Access-Control-Allow-Origin", "http://localhost:5173");
+        res.set_header("Access-Control-Allow-Origin", allowed_origins);
         res.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.set_header("Access-Control-Allow-Headers", "Content-Type");
     }
